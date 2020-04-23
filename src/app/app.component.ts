cbd1940ee4b537d6ad182b9867acc6c2e486acdc
tonghua <1742573849@qq.com>
2020-04-23 16:15:38 +0800

    111

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, AbstractControl, Validators, FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+function userNameValidator(control:FormControl):{[s:string]:boolean}{
+  if( !control.value.match(/^t/)){
+    return{ invalidUser:true};
+  }
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  myForm:FormGroup;
+
+  userName:AbstractControl;
+  password:AbstractControl;
+
+  name$:Observable<string>;
+  constructor(private fb:FormBuilder){
+    this.myForm = this.fb.group(
+      {
+        'userName':['tonghua',Validators.compose([Validators.required,userNameValidator])],
+        'password':['',Validators.compose([Validators.required,Validators.minLength(6)])]
+      }
+    );
+    this.userName = this.myForm.controls['userName'];
+    this.password = this.myForm.controls['password'];
+    this.name$ = this.userName.valueChanges;
+    this.userName.valueChanges.subscribe(val =>{
+      console.log(val);
+    })
+  }
+  onSubmit(value:any){
+    console.log(value);
+  }
+}
